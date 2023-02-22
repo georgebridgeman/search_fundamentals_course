468899f87eab3d7d489beac0aa92e74519bee145
George Bridgeman <georgebridgeman@users.noreply.github.com>
2023-02-22 22:24:21 +0000

    Add week 2 solutions.

--- week2/search.py
+++ week2/search.py
@@ -60,15 +60,24 @@ def autocomplete():
     results = {}
     if request.method == 'GET':  # a query has been submitted
         prefix = request.args.get("prefix")
-        print(f"Prefix: {prefix}")
         if prefix is not None:
             type = request.args.get("type", "queries") # If type == queries, this is an autocomplete request, else if products, it's an instant search request.
             ##### W2, L3, S1
-            search_response = None
-            print("TODO: implement autocomplete AND instant search")
+            suggester_obj = {
+                "suggest": {
+                    "autocomplete": {
+                        "prefix": prefix,
+                        "completion": {
+                            "skip_duplicates": True,
+                            "field": "suggest"
+                        }
+                    }
+                }
+            }
+            index = "bbuy_queries" if type == "queries" else "bbuy_products"
+            search_response = get_opensearch().search(body=suggester_obj, index=index)
             if (search_response and search_response['suggest']['autocomplete'] and search_response['suggest']['autocomplete'][0]['length'] > 0): # just a query response
                 results = search_response['suggest']['autocomplete'][0]['options']
-    print(f"Results: {results}")
     return {"completions": results}
 
 @bp.route('/query', methods=['GET', 'POST'])
